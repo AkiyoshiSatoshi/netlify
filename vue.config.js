e8e9fb7c459f59354489a802f2e10da15e720e40
AkiyoshiSatoshi <akiyoshiakiyoshi3104@gmail.com>
2021-08-20 15:12:09 +0900

    vue.config.js fixed

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   devServer: {
-    proxy: 'https://api.coin.z.com'
+    proxy: 'https://api.coin.z.com',
+    compress: true,
+    disableHostCheck: true
   },
   publicPath: './',
 };
